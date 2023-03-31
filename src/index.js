0e55c521995fb0a11ddae0d69988f7a3f642d266
Diana Goering <goeringdi@gmail.com>
2023-03-31 22:56:36 +0200

    name

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const getUsers = require ("./modules/users");
 
 const hostname = "127.0.0.1";
 const port = 3003;
-const name = 'name';
 
 
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const params = url.searchParams;
+    const name = params.get('hello');
+
+    
     if (req.url === `/hello=${name}`) {
         if (name === '') {
             res.statusCode = 400;
@@ -28,12 +33,21 @@ const server = http.createServer((req, res) => {
         res.write(getUsers())
         res.end()
     }
-    res.statusCode = 200;
-    res.statusMessage = "OK";
+    if (![...params].length) {
+        res.statusCode = 200;
+        res.statusMessage = "OK";
+        res.setHeader = "Content-Type", "text/plain";
+        res.write('Hello world');
+        res.end();
+        
+        return;
+    }
+    res.statusCode = 500;
+    res.statusMessage = "Internal Server Error";
     res.setHeader = "Content-Type", "text/plain";
-    res.write('Hello world')
+    res.write('')
     res.end()
 });
 server.listen(port, hostname, () => {
-  console.log("Сервер запущен по адресу http://${hostname}:${port}/");
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
