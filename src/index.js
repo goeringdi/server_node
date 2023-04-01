31e558299570b662285fe71cdb13eaaf32ed63fb
Diana Goering <goeringdi@gmail.com>
2023-04-01 18:41:49 +0200

    error

--- src/index.js
+++ src/index.js
@@ -13,27 +13,31 @@ const server = http.createServer(async (req, res) => {
 
     
     if (req.url === `/hello=${name}`) {
-        if (name === '') {
-            res.statusCode = 400;
-            res.statusMessage = "Error";
-            res.setHeader = "Content-Type", "text/plain";
-            res.write('Enter a name')
-            res.end()
-        }
-        res.statusCode = 200;
-        res.statusMessage = "OK";
-        res.setHeader = "Content-Type", "text/plain";
-        res.write(`Hello,${name}`)
-        res.end()        
+        if (name) {
+            if (name === "") {
+              res.statusCode = 400;
+              res.statusMessage = "Error";
+              (res.setHeader = "Content-Type"), "text/plain";
+              res.write("Enter a name");
+              res.end();
+              return;
+            }
+            res.statusCode = 200;
+            res.statusMessage = "OK";
+            (res.setHeader = "Content-Type"), "text/plain";
+            res.write(`Hello,${name}`);
+            res.end();
+            return;
+          }
     }
-    if (req.url === '/users') {
+    if (req.url === '/?users') {
         res.statusCode = 200;
         res.statusMessage = "OK";
         res.setHeader = "Content-Type: application/json";
         res.write(getUsers())
         res.end()
     }
-    if (![...params].length) {
+    if ([...params].length) {
         res.statusCode = 200;
         res.statusMessage = "OK";
         res.setHeader = "Content-Type", "text/plain";
